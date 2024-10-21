8a77280206fdb0123fa2f6e45b4b96612e73b097
Ansel Joseph <anseljoseph2509@gmail.com>
2024-10-21 16:14:29 +0530

    add: greater sum/pdt

--- questions.cpp
+++ questions.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 int main()
 {
-int num1=8;
-double num2=6.6;
-float num3=3.9;
-
-double result1=num1*num2;
-double finalresult=result1+num3;
-cout<<"Before typecasting: "<<double(result1)<<endl;
-cout<<"After typecasting: "<<int(result1)<<endl;
-cout<<"Integer: "<<num1<<endl;
-cout<<"Double: "<<num2<<endl;
-cout<<"Float: "<<num3<<endl;
-cout<<"Multiplication Result: "<<result1<<endl;
-cout<<"Final Result"<<finalresult<<endl;
-
-
+    cout<<"Enter three numbers";
+int num1,num2,num3,sum,difference,product;
+cin>>num1>>num2>>num3;
+sum=num1+num2+num3;
+difference=num1-num2-num3;
+product=num1*num2*num3;
+cout<<"The sum is "<<sum<<endl;
+cout<<"The product is "<<product<<endl;
+cout<<"The difference is "<<difference<<endl;
+if(sum>product){
+cout<<"Sum is greater "<<endl;
+}else if(product>sum){
+    cout<<"Product is greater";
+}else{
+    cout<<"Both the sum and product are equal";
+}
     return 0;
 }
