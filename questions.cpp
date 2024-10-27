6f66ca48e285d79b82b421b04dbe3f636dfcc69c
Ansel Joseph <anseljoseph2509@gmail.com>
2024-10-27 22:45:21 +0530

    add: strings

--- questions.cpp
+++ questions.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+
 int main() {
-    int rows, cols;
+    char str1[100], str2[100];
 
-    cout << "Enter number of rows: ";
-    cin >> rows;
-    cout << "Enter number of columns: ";
-    cin >> cols;
+ 
+    cout << "\n\nEnter the first string: ";
+    cin >> str1;
+    cout << "Enter the second string: ";
+    cin >> str2;
 
-    int matrix[rows][cols], transpose[cols][rows];
 
+    cout << "\nLength of first string: " << strlen(str1) << endl;
+    cout << "Length of second string: " << strlen(str2) << endl;
 
-    cout << "Enter elements of the matrix row-wise:\n";
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            cin >> matrix[i][j];
-        }
+    char concatenated[200];
+    strcpy(concatenated, str1);
+    strcat(concatenated, str2);
+    cout << "\nConcatenated string: " << concatenated << endl;
+
+  
+    if (strcmp(str1, str2) == 0) {
+        cout << "\nThe strings are equal." << endl;
+    } else {
+        cout << "\nThe strings are not equal." << endl;
     }
 
+   
+    char temp[100];
+    strcpy(temp, str1);
+    strcpy(str1, str2);
+    strcpy(str2, temp);
+    cout << "\nAfter swapping:" << endl;
+    cout << "First string: " << str1 << endl;
+    cout << "Second string: " << str2 << endl;
 
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            transpose[j][i] = matrix[i][j];
-        }
-    }
-
-
-    cout << "\nTranspose of the matrix:\n";
-    for (int i = 0; i < cols; ++i) {
-        for (int j = 0; j < rows; ++j) {
-            cout << transpose[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    return 0;
+   return 0;
 }
+
